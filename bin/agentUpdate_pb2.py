31bda3fc5cf3f2c96496c8d1c504097cf90cd6a9
Ryan <r.mckee@qub.ac.uk>
2025-02-05 17:10:32 +0000

    UPDATE: obstacle updates

--- bin/agentUpdate_pb2.py
+++ bin/agentUpdate_pb2.py
@@ -19,36 +19,36 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto2',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x11\x61gentUpdate.proto\x12\x05\x63olav\"\x96\x02\n\x0b\x41gentUpdate\x12\x13\n\x0bmission_tag\x18\x01 \x02(\t\x12\x11\n\tagent_tag\x18\x02 \x02(\t\x12\'\n\x05state\x18\x03 \x02(\x0b\x32\x18.colav.AgentUpdate.State\x12\x11\n\ttimestamp\x18\x04 \x02(\t\x12\x10\n\x08timestep\x18\x05 \x02(\t\x1a\'\n\x04Pose\x12\t\n\x01x\x18\x01 \x02(\x02\x12\t\n\x01y\x18\x02 \x02(\x02\x12\t\n\x01z\x18\x03 \x02(\x02\x1ah\n\x05State\x12%\n\x04pose\x18\x01 \x02(\x0b\x32\x17.colav.AgentUpdate.Pose\x12\x10\n\x08velocity\x18\x02 \x02(\x02\x12\x10\n\x08yaw_rate\x18\x03 \x02(\x02\x12\x14\n\x0c\x61\x63\x63\x65leration\x18\x04 \x02(\x02'
+  serialized_pb=b'\n\x11\x61gentUpdate.proto\x12\x05\x63olav\"\xcc\x03\n\x0b\x41gentUpdate\x12\x13\n\x0bmission_tag\x18\x01 \x02(\t\x12\x11\n\tagent_tag\x18\x02 \x02(\t\x12\'\n\x05state\x18\x03 \x02(\x0b\x32\x18.colav.AgentUpdate.State\x12\x11\n\ttimestamp\x18\x04 \x02(\t\x12\x10\n\x08timestep\x18\x05 \x02(\t\x1a\xdc\x01\n\x04Pose\x12\x32\n\x08position\x18\x01 \x02(\x0b\x32 .colav.AgentUpdate.Pose.Position\x12\x38\n\x0borientation\x18\x02 \x02(\x0b\x32#.colav.AgentUpdate.Pose.Orientation\x1a+\n\x08Position\x12\t\n\x01x\x18\x01 \x02(\x02\x12\t\n\x01y\x18\x02 \x02(\x02\x12\t\n\x01z\x18\x03 \x02(\x02\x1a\x39\n\x0bOrientation\x12\t\n\x01x\x18\x01 \x02(\x02\x12\t\n\x01y\x18\x02 \x02(\x02\x12\t\n\x01z\x18\x03 \x02(\x02\x12\t\n\x01w\x18\x04 \x02(\x02\x1ah\n\x05State\x12%\n\x04pose\x18\x01 \x02(\x0b\x32\x17.colav.AgentUpdate.Pose\x12\x10\n\x08velocity\x18\x02 \x02(\x02\x12\x10\n\x08yaw_rate\x18\x03 \x02(\x02\x12\x14\n\x0c\x61\x63\x63\x65leration\x18\x04 \x02(\x02'
 )
 
 
 
 
-_AGENTUPDATE_POSE = _descriptor.Descriptor(
-  name='Pose',
-  full_name='colav.AgentUpdate.Pose',
+_AGENTUPDATE_POSE_POSITION = _descriptor.Descriptor(
+  name='Position',
+  full_name='colav.AgentUpdate.Pose.Position',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='x', full_name='colav.AgentUpdate.Pose.x', index=0,
+      name='x', full_name='colav.AgentUpdate.Pose.Position.x', index=0,
       number=1, type=2, cpp_type=6, label=2,
       has_default_value=False, default_value=float(0),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='y', full_name='colav.AgentUpdate.Pose.y', index=1,
+      name='y', full_name='colav.AgentUpdate.Pose.Position.y', index=1,
       number=2, type=2, cpp_type=6, label=2,
       has_default_value=False, default_value=float(0),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='z', full_name='colav.AgentUpdate.Pose.z', index=2,
+      name='z', full_name='colav.AgentUpdate.Pose.Position.z', index=2,
       number=3, type=2, cpp_type=6, label=2,
       has_default_value=False, default_value=float(0),
       message_type=None, enum_type=None, containing_type=None,
@@ -66,8 +66,98 @@ _AGENTUPDATE_POSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=162,
-  serialized_end=201,
+  serialized_start=281,
+  serialized_end=324,
+)
+
+_AGENTUPDATE_POSE_ORIENTATION = _descriptor.Descriptor(
+  name='Orientation',
+  full_name='colav.AgentUpdate.Pose.Orientation',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='x', full_name='colav.AgentUpdate.Pose.Orientation.x', index=0,
+      number=1, type=2, cpp_type=6, label=2,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='y', full_name='colav.AgentUpdate.Pose.Orientation.y', index=1,
+      number=2, type=2, cpp_type=6, label=2,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='z', full_name='colav.AgentUpdate.Pose.Orientation.z', index=2,
+      number=3, type=2, cpp_type=6, label=2,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='w', full_name='colav.AgentUpdate.Pose.Orientation.w', index=3,
+      number=4, type=2, cpp_type=6, label=2,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto2',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=326,
+  serialized_end=383,
+)
+
+_AGENTUPDATE_POSE = _descriptor.Descriptor(
+  name='Pose',
+  full_name='colav.AgentUpdate.Pose',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='position', full_name='colav.AgentUpdate.Pose.position', index=0,
+      number=1, type=11, cpp_type=10, label=2,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='orientation', full_name='colav.AgentUpdate.Pose.orientation', index=1,
+      number=2, type=11, cpp_type=10, label=2,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[_AGENTUPDATE_POSE_POSITION, _AGENTUPDATE_POSE_ORIENTATION, ],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto2',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=163,
+  serialized_end=383,
 )
 
 _AGENTUPDATE_STATE = _descriptor.Descriptor(
@@ -118,8 +208,8 @@ _AGENTUPDATE_STATE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=203,
-  serialized_end=307,
+  serialized_start=385,
+  serialized_end=489,
 )
 
 _AGENTUPDATE = _descriptor.Descriptor(
@@ -178,9 +268,13 @@ _AGENTUPDATE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=29,
-  serialized_end=307,
+  serialized_end=489,
 )
 
+_AGENTUPDATE_POSE_POSITION.containing_type = _AGENTUPDATE_POSE
+_AGENTUPDATE_POSE_ORIENTATION.containing_type = _AGENTUPDATE_POSE
+_AGENTUPDATE_POSE.fields_by_name['position'].message_type = _AGENTUPDATE_POSE_POSITION
+_AGENTUPDATE_POSE.fields_by_name['orientation'].message_type = _AGENTUPDATE_POSE_ORIENTATION
 _AGENTUPDATE_POSE.containing_type = _AGENTUPDATE
 _AGENTUPDATE_STATE.fields_by_name['pose'].message_type = _AGENTUPDATE_POSE
 _AGENTUPDATE_STATE.containing_type = _AGENTUPDATE
@@ -191,6 +285,20 @@ _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 AgentUpdate = _reflection.GeneratedProtocolMessageType('AgentUpdate', (_message.Message,), {
 
   'Pose' : _reflection.GeneratedProtocolMessageType('Pose', (_message.Message,), {
+
+    'Position' : _reflection.GeneratedProtocolMessageType('Position', (_message.Message,), {
+      'DESCRIPTOR' : _AGENTUPDATE_POSE_POSITION,
+      '__module__' : 'agentUpdate_pb2'
+      # @@protoc_insertion_point(class_scope:colav.AgentUpdate.Pose.Position)
+      })
+    ,
+
+    'Orientation' : _reflection.GeneratedProtocolMessageType('Orientation', (_message.Message,), {
+      'DESCRIPTOR' : _AGENTUPDATE_POSE_ORIENTATION,
+      '__module__' : 'agentUpdate_pb2'
+      # @@protoc_insertion_point(class_scope:colav.AgentUpdate.Pose.Orientation)
+      })
+    ,
     'DESCRIPTOR' : _AGENTUPDATE_POSE,
     '__module__' : 'agentUpdate_pb2'
     # @@protoc_insertion_point(class_scope:colav.AgentUpdate.Pose)
@@ -209,6 +317,8 @@ AgentUpdate = _reflection.GeneratedProtocolMessageType('AgentUpdate', (_message.
   })
 _sym_db.RegisterMessage(AgentUpdate)
 _sym_db.RegisterMessage(AgentUpdate.Pose)
+_sym_db.RegisterMessage(AgentUpdate.Pose.Position)
+_sym_db.RegisterMessage(AgentUpdate.Pose.Orientation)
 _sym_db.RegisterMessage(AgentUpdate.State)
 
 
